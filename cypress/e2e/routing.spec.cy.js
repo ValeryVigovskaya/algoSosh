820b2dd7db39db53c41e88c2e89a8f317f0e5e2e
Valeria Vigovskaya <vigovskaya.lera@yandex.ru>
2023-10-07 13:55:58 +0300

    тестирование визуализации строки

--- cypress/e2e/routing.spec.cy.js
+++ cypress/e2e/routing.spec.cy.js
@@ -1,42 +1,44 @@
-export const baseURL = 'http://localhost:3000';
 describe('app works correctly with routes', function () {
     before(function () {
-        cy.visit(baseURL);
-    });
+        cy.visit('/');
+    })
 
     it('should open a page default', function () {
         cy.contains('МБОУ АЛГОСОШ');
     });
 
     it('should open a page with a string algorithm', function () {
-        cy.visit(`${baseURL}/recursion`);
-        cy.contains("Строка");
+        // cy.visit(`/recursion`);
+        // cy.contains("Строка");
+        cy.visit('/');
+        cy.get(`[href="/recursion"]`).click();
+        cy.location('pathname').should('eq', '/recursion')
     });
 
-    it('should open a page with the algorithm Fibonacci sequence', function () {
-        cy.visit(`${baseURL}/fibonacci`);
-        cy.contains("Последовательность Фибоначчи");
-    });
+    // it('should open a page with the algorithm Fibonacci sequence', function () {
+    //     cy.visit(`/fibonacci`);
+    //     cy.contains("Последовательность Фибоначчи");
+    // });
 
-    it('should open a page with sorting algorithms', function () {
-        cy.visit(`${baseURL}/sorting`);
-        cy.contains("Сортировка массива");
-    });
+    // it('should open a page with sorting algorithms', function () {
+    //     cy.visit(`/sorting`);
+    //     cy.contains("Сортировка массива");
+    // });
 
-    it('should open a page with a stack algorithm', function () {
-        cy.visit(`${baseURL}/stack`);
-        cy.contains("Стек");
-    });
+    // it('should open a page with a stack algorithm', function () {
+    //     cy.visit(`/stack`);
+    //     cy.contains("Стек");
+    // });
 
-    it('should open a page with a queue algorithm', function () {
-        cy.visit(`${baseURL}/queue`);
-        cy.contains("Очередь");
-    });
+    // it('should open a page with a queue algorithm', function () {
+    //     cy.visit(`/queue`);
+    //     cy.contains("Очередь");
+    // });
 
-    it('should open a page with a list algorithm', function () {
-        cy.visit(`${baseURL}/list`);
-        cy.contains("Связный список");
-    });
+    // it('should open a page with a list algorithm', function () {
+    //     cy.visit(`/list`);
+    //     cy.contains("Связный список");
+    // });
 
     // it('should open agreement page after continue button click', function() {
     //   cy.contains('Обычная доставка').click();
--- cypress/e2e/string.spec.cy.js
+++ cypress/e2e/string.spec.cy.js
@@ -1,14 +1,13 @@
-import { baseURL } from "./routing.spec.cy"
-
+import { DELAY_IN_MS } from "../../src/constants/delays"
 describe('products management works correctly', function () {
     before(function () {
-        cy.visit(`${baseURL}/recursion`);
+        cy.visit('/recursion');
     })
 
     it('should be an inactive button if the Input is empty', function () {
-        cy.get('[class^=string_input_container__]').first().as('container');
-        cy.get('@container').find('[class^=input_input]').first().as('input');
-        cy.get('@container').find('button').last().as('button');
+        cy.get('[class^=string_input_container__]').as('container');
+        cy.get('@container').find('[class^=input_input]').as('input');
+        cy.get('@container').find('button').as('button');
 
         cy.get('@input').should('be.empty');
         cy.get('@button').should('be.disabled');
@@ -17,25 +16,62 @@ describe('products management works correctly', function () {
 
 describe('correct turn of the line', function () {
     before(function () {
-        cy.visit(`${baseURL}/recursion`);
-    })
+        cy.visit('/recursion');
+        cy.get('input').type('Hello');
+        cy.get('button').should('not.be.disabled').contains('Развернуть').click();
+    });
 
     it('should be the correct string reversal', function () {
-        cy.get('[class^=string_input_container__]').first().as('container');
-        cy.get('@container').find('[class^=input_input]').first().as('input');
-        cy.get('@container').find('button').last().as('button');
-        cy.get('[class^=string_circle_container__]').first().as('circle_container');
+        cy.get('ul>li').as('array');
 
+        //проверка перебором на дефолтное состояние
+        cy.get('@array')
+            .should('have.length', 5)
+            .each(($li) => {
+                cy.get($li).find('[class^=circle_circle__]').as('circle');
+                cy.get('@circle').should('have.css', 'border-color', 'rgb(0, 50, 255)');
+            });
 
+        //проверка перебором первого и последнего элементов на изменение
+        cy.get('@array')
+            .should('have.length', 5)
+            .each(($li, index, list) => {
+                cy.get($li).find('[class^=circle_circle__]').as('circle');
+                if (index === 0 || index === (list.length - 1)) {
+                    cy.get('@circle').should('have.css', 'border-color', 'rgb(210, 82, 225)');
+                }
+            });
+        //следующий шаг изменения цвета
+        cy.get('@array')
+            .should('have.length', 5)
+            .each(($li, index, list) => {
+                cy.get($li).find('[class^=circle_circle__]').as('circle');
+                if (index === 0 || index === (list.length - 1)) {
+                    cy.get('@circle').should('have.css', 'border-color', 'rgb(127, 224, 81)');
+                }
+                if (index === 1 || index === (list.length - 2)) {
+                    cy.get('@circle').should('have.css', 'border-color', 'rgb(210, 82, 225)');
+                }
+            });
+        //следующий шаг изменения цвета
+        cy.get('@array')
+            .should('have.length', 5)
+            .each(($li, index, list) => {
+                cy.get($li).find('[class^=circle_circle__]').as('circle');
+                if (index === 0 || index === (list.length - 1)) {
+                    cy.get('@circle').should('have.css', 'border-color', 'rgb(127, 224, 81)');
+                }
+                if (index === 1 || index === (list.length - 2)) {
+                    cy.get('@circle').should('have.css', 'border-color', 'rgb(210, 82, 225)');
+                }
+            });
 
-        cy.get('@input').type('Hello');
-        cy.get('@button').should('not.be.disabled');
-
-        cy.get('@button').click();
-        cy.get('ul>li')
-        .should('have.length', 5)
-        // .each(($li, index, $lis) => {
-        //     cy.wrap($li).get('[class^=circle_default]')
-        //   })
+        //финальный шаг
+        cy.get('@array')
+            .should('have.length', 5)
+            .each(($li) => {
+                cy.get($li).find('[class^=circle_circle__]').as('circle');
+                cy.get('@circle').should('have.css', 'border-color', 'rgb(127, 224, 81)');
+            });
     });
 })
